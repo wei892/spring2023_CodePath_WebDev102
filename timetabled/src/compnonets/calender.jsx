c4093bbed1cd10e8314e68e19c2fe0baf407a45a
Wei <wlin.li2403@gmail.com>
2023-02-25 12:14:55 -0500

    completed lab_01

--- timetabled/src/compnonets/calender.jsx
+++ timetabled/src/compnonets/calender.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Event from "./Event";
 
 const Calendar =() => {
 return(
@@ -6,6 +7,7 @@ return(
         <table>
             <thead>
                 <tr>
+                    <th></th>
                     <th> Sunday </th>
                     <th> Monday </th>
                     <th> Tuesday </th>
@@ -16,7 +18,115 @@ return(
                 </tr>
             </thead>
             <tbody>
+                <td className="time"> 8 am </td>
+                <Event event='Fancy Dinner' color='green'/>
+                <Event event='Class' color='pink' /> 
+                <Event event='Class' color='pink' />               
+                <Event event='Class' color='pink' /> 
+                <Event event='Class' color='pink' /> 
                 <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 9 am </td>
+                <td></td>
+                <Event event='Class' color='pink' />
+                <Event event='Class' color='pink' /> 
+                <Event event='Class' color='pink' /> 
+                <Event event='Class' color='pink' /> 
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 10 am </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 11 am </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 12 pm </td>
+                <td></td>
+                <Event event='Study' color='blue' location ='library' /> 
+                <Event event='Study' color='blue'location='Blue Cafe'/>                 
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 1 pm </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <Event event='Study' color='blue'/> 
+                <Event event='Study' color='blue'/> 
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 2 pm </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 3 pm </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 4 pm </td>
+                <td></td>
+                <td></td>
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+            <tbody>
+                <td className="time"> 5 pm </td>
                 <td></td>
                 <td></td>
                 <td></td>                
